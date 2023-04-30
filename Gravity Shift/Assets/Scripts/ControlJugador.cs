79935525a430d5612fc42cedaf7d60c1f378e458
Syna02 <123471603+Syna02@users.noreply.github.com>
2023-04-30 14:06:06 +0200

    Implementacion de salto y doble salto

--- Gravity Shift/Assets/Scripts/ControlJugador.cs
+++ Gravity Shift/Assets/Scripts/ControlJugador.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class ControlJugador : MonoBehaviour
 {
     public float velocidadMovimiento;
+
     public Rigidbody2D rB;
+
+    public float potenciaSalto;
+    public bool puedeSaltarDoble;
+
+    public bool estaEnSuelo;
+    public Transform detectorSuelo;
+    public LayerMask esSuelo;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +24,29 @@ public class ControlJugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rB.velocity = new Vector2(velocidadMovimiento * Input.GetAxis("Horizontal"), rB.velocity.y);
+        rB.velocity = new Vector2(velocidadMovimiento * Input.GetAxisRaw("Horizontal"), rB.velocity.y);
+
+        estaEnSuelo = Physics2D.OverlapCircle(detectorSuelo.position, .2f, esSuelo);
+
+        if (estaEnSuelo)
+        {
+            puedeSaltarDoble = true;
+        }
+
+        if (Input.GetButtonDown("Jump")) 
+        {
+            if (estaEnSuelo)
+            {
+                rB.velocity = new Vector2(rB.velocity.x, potenciaSalto);
+            }
+            else
+            {
+                if (puedeSaltarDoble)
+                {
+                    rB.velocity = new Vector2(rB.velocity.x, potenciaSalto);
+                    puedeSaltarDoble = false;
+                }
+            }
+        }
     }
 }
