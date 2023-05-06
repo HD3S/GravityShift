8c57a9f2b2587b64ee76ab82304a468b846c3ed3
HD3S <98744578+HD3S@users.noreply.github.com>
2023-05-06 21:51:54 +0200

    Escenario Modificado

--- Gravity Shift/Assets/Scripts/ControlVidaJugador.cs
+++ Gravity Shift/Assets/Scripts/ControlVidaJugador.cs
@@ -20,7 +20,6 @@ public class ControlVidaJugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
